be1f59a6a20375e6ee84d358594c3115517e7a39
Mikapuccino <nelsonfcmilheiro@gmail.com>
2023-05-27 23:06:15 +0100

    Fix SpellPhase method, invalid choices no longer crash the program, add comments to SpellPhase method

--- TragicTheReckoning/Controller.cs
+++ TragicTheReckoning/Controller.cs
@@ -50,22 +50,46 @@ namespace TragicTheReckoning
             
             for (int i = 0; i < 2; i++)
             {
+                // When the player has 0 MP,
+                // passes to other player or ends the phase
                 while (players[i].MP > 0)
                 {
+                    // WriteLine should go to View, in here for testing
+                    Console.WriteLine(i + 1);
+                    Console.WriteLine(players[i].MP);
+                    Console.WriteLine(players[i].Hand.Count());
+                    
                     // ReadLine should go to View, in here for testing
                     cardChosen = int.Parse(Console.ReadLine());
 
-                    if (players[i].Hand[cardChosen - 1].Cost <= players[i].MP)
+                    // If player choose a card in a valid position in his hand
+                    if (cardChosen <= players[i].Hand.Count())
                     {
-                        if (i == 0)
+                        // If the card cost is less or equal
+                        // to the player current MP
+                        if (players[i].Hand[cardChosen - 1].Cost <= players[i].MP)
                         {
-                            field1.Add(players[i].Hand[cardChosen - 1]);
-                        }
-                        else field2.Add(players[i].Hand[cardChosen - 1]);
+                            // Puts the card in the correct field
+                            if (i == 0)
+                            {
+                                field1.Add(players[i].Hand[cardChosen - 1]);
+                            }
+                            else field2.Add(players[i].Hand[cardChosen - 1]);
 
-                        players[i].Hand.RemoveAt(cardChosen - 1);
-                        players[i].MP -= players[i].Hand[cardChosen - 1].Cost;
+                            // Reduces player MP by the cost of the card played
+                            players[i].MP -= 
+                            players[i].Hand[cardChosen - 1].Cost;
+
+                            // Removes card from the hand
+                            players[i].Hand.RemoveAt(cardChosen - 1);
+                        }
+
+                        // WriteLine should go to View, in here for testing
+                        else Console.WriteLine("Not enough mana");
                     }
+
+                    // WriteLine should go to View, in here for testing
+                    else Console.WriteLine("Not a valid option");
                 }
             }
         }
